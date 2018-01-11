063eb6d6305d29b9e9d8d9a55994c548eb71cc38
Elena Myagkova <spban@mail.ru>
2018-01-11 09:58:12 +0300

    function minus

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function plus(){
 	result = num1 + num2;
 	document.getElementById("out").innerHTML = result;
 }
+
+function minus(){
+		var num1, num2, result;
+	num1 = document.getElementById("num1").value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById("num2").value;
+	num2 = parseInt(num2);
+
+	result = num1 - num2;
+	document.getElementById("out").innerHTML = result;
+}
